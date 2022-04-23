d840d168562a9cf46dd5519551bd8f6afc22e123
DenizPLT <80721116+DenizPLT@users.noreply.github.com>
2022-04-23 18:21:46 +0200

    Pruefe auf Niederlage

--- My2048Game.py
+++ My2048Game.py
@@ -13,6 +13,9 @@ class My2048Game:
         while (True):
             self.ausgabe_spielfeld()
             self.score_ausgeben()
+            if (self.pruefe_verloren() == True):
+                print("Keine gueltigen Zuege mehr moeglich! Spiel vorbei!")
+                break
             spielzug = input(
                 "Wie Spielfeld verschieben? L, R, O, U? (Quit = Q)")
             tempBoard = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
@@ -43,7 +46,7 @@ class My2048Game:
                 self.verschiebeRunter(3)
             else:
                 print("Inkorrekte Eingabe! Versuche es erneut")
-            if(self.vergleiche_board(self.board, tempBoard) == False):
+            if (self.vergleiche_board(self.board, tempBoard) == False):
                 self.generiere_zahl()
 
     def generiere_zahl(self):
@@ -59,11 +62,11 @@ class My2048Game:
         for i in range(len(self.board)):
             for j in range(len(self.board)):
                 abstand = "    "
-                if (self.board[i][j] >9):
+                if (self.board[i][j] > 9):
                     abstand = "   "
-                if (self.board[i][j] >99):
+                if (self.board[i][j] > 99):
                     abstand = "  "
-                if (self.board[i][j] >999):
+                if (self.board[i][j] > 999):
                     abstand = " "
                 print(self.board[i][j], end=abstand)
             print("")
@@ -71,65 +74,65 @@ class My2048Game:
     def verschiebeHoch(self, spalte):
         for reihe in range(1, 4):
             sucheReihe = reihe - 1
-            while(self.board[sucheReihe][spalte] == 0 and sucheReihe > 0): #sucht von oben nach unten nach nullen
+            while (self.board[sucheReihe][spalte] == 0 and sucheReihe > 0):  # sucht von oben nach unten nach nullen
                 sucheReihe = sucheReihe - 1
-            if (sucheReihe == 0 and self.board[sucheReihe][spalte] == 0): #verschiebt über die nullen
+            if (sucheReihe == 0 and self.board[sucheReihe][spalte] == 0):  # verschiebt über die nullen
                 self.board[0][spalte] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
-            elif(self.board[sucheReihe][spalte] == self.board[reihe][spalte]): #zwei gleiche zahlen gefunden
+            elif (self.board[sucheReihe][spalte] == self.board[reihe][spalte]):  # zwei gleiche zahlen gefunden
                 self.zusammenfuegen(reihe, spalte, sucheReihe, spalte)
                 self.board[reihe][spalte] = 0
-            elif(reihe > sucheReihe + 1): # zahl gefunden aber übereinstimmen nicht
-                self.board[sucheReihe+1][spalte] = self.board[reihe][spalte]
+            elif (reihe > sucheReihe + 1):  # zahl gefunden aber übereinstimmen nicht
+                self.board[sucheReihe + 1][spalte] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
 
     def verschiebeRunter(self, spalte):
         for reihe in range(2, -1, -1):
             sucheReihe = reihe + 1
-            while(self.board[sucheReihe][spalte] == 0 and sucheReihe < 3):
+            while (self.board[sucheReihe][spalte] == 0 and sucheReihe < 3):
                 sucheReihe = sucheReihe + 1
             if (sucheReihe == 3 and self.board[sucheReihe][spalte] == 0):
                 self.board[3][spalte] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
-            elif(self.board[sucheReihe][spalte] == self.board[reihe][spalte]):
+            elif (self.board[sucheReihe][spalte] == self.board[reihe][spalte]):
                 self.zusammenfuegen(reihe, spalte, sucheReihe, spalte)
                 self.board[reihe][spalte] = 0
-            elif(reihe < sucheReihe - 1): # zahl gefunden aber übereinstimmen nicht
-                self.board[sucheReihe-1][spalte] = self.board[reihe][spalte]
+            elif (reihe < sucheReihe - 1):  # zahl gefunden aber übereinstimmen nicht
+                self.board[sucheReihe - 1][spalte] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
 
     def verschiebeLinks(self, reihe):
         for spalte in range(1, 4):
             sucheSpalte = spalte - 1
-            while(self.board[reihe][sucheSpalte] == 0 and sucheSpalte > 0):
+            while (self.board[reihe][sucheSpalte] == 0 and sucheSpalte > 0):
                 sucheSpalte = sucheSpalte - 1
             if (sucheSpalte == 0 and self.board[reihe][sucheSpalte] == 0):
                 self.board[reihe][0] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
-            elif(self.board[reihe][sucheSpalte] == self.board[reihe][spalte]):
+            elif (self.board[reihe][sucheSpalte] == self.board[reihe][spalte]):
                 self.zusammenfuegen(reihe, spalte, reihe, sucheSpalte)
                 self.board[reihe][spalte] = 0
-            elif(spalte > sucheSpalte + 1): # zahl gefunden aber übereinstimmen nicht
-                self.board[reihe][sucheSpalte+1] = self.board[reihe][spalte]
+            elif (spalte > sucheSpalte + 1):  # zahl gefunden aber übereinstimmen nicht
+                self.board[reihe][sucheSpalte + 1] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
 
     def verschiebeRechts(self, reihe):
         for spalte in range(2, -1, -1):
             sucheSpalte = spalte + 1
-            while(self.board[reihe][sucheSpalte] == 0 and sucheSpalte < 3):
+            while (self.board[reihe][sucheSpalte] == 0 and sucheSpalte < 3):
                 sucheSpalte = sucheSpalte + 1
             if (sucheSpalte == 3 and self.board[reihe][sucheSpalte] == 0):
                 self.board[reihe][3] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
-            elif(self.board[reihe][sucheSpalte] == self.board[reihe][spalte]):
+            elif (self.board[reihe][sucheSpalte] == self.board[reihe][spalte]):
                 self.zusammenfuegen(reihe, spalte, reihe, sucheSpalte)
                 self.board[reihe][spalte] = 0
-            elif(spalte < sucheSpalte - 1): # zahl gefunden aber übereinstimmen nicht
-                self.board[reihe][sucheSpalte-1] = self.board[reihe][spalte]
+            elif (spalte < sucheSpalte - 1):  # zahl gefunden aber übereinstimmen nicht
+                self.board[reihe][sucheSpalte - 1] = self.board[reihe][spalte]
                 self.board[reihe][spalte] = 0
 
     def zusammenfuegen(self, reihe1, spalte1, reihe2, spalte2):
-        self.board[reihe2][spalte2] = 2*self.board[reihe2][spalte2]
+        self.board[reihe2][spalte2] = 2 * self.board[reihe2][spalte2]
         self.aktualisiere_score(self.board[reihe2][spalte2])
 
     def vergleiche_board(self, board1, board2):
@@ -146,9 +149,19 @@ class My2048Game:
         print("Score: {} \n".format(self.score))
 
     def pruefe_verloren(self):
-        ##
-        pass
-
+        for reihe in range(4):
+            for spalte in range(4):
+                if (self.board[reihe][spalte] == 0):  # pruefe ob noch nullen vorhanden
+                    return False
+        for reihe in range(4):
+            for spalte in range(3):
+                if (self.board[reihe][spalte] == self.board[reihe][spalte + 1]):  # pruefe ob horizontale zuege moeglich
+                    return False
+        for reihe in range(3):
+            for spalte in range(4):
+                if (self.board[reihe][spalte] == self.board[reihe + 1][spalte]):  # pruefe ob vertikale zuege moeglich
+                    return False
+        return True
 
 
 game = My2048Game()
